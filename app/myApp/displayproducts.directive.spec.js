0e5da3067a651c39eb081405e21c38510415fb61
Rob Moore <me@robdmoore.id.au>
2015-04-21 22:33:29 +0800

    Added basic tests for the directive

---
+++ app/myApp/displayproducts.directive.spec.js
@@ -0,0 +1,17 @@
+﻿"use strict";
+
+describe("myapp-display-products", function () {
+
+    beforeEach(module("myApp"));
+    beforeEach(DirectiveFixture.init);
+
+    it("should output product information", function () {
+        HttpMocks.product.cars(DirectiveFixture);
+
+        DirectiveFixture.verify(heredoc(function () {/*    
+            <myapp-display-products category="car" product="car">
+                <div>{{car.name}}</div>
+            </myapp-display-products>
+        */}));
+    });
+});
---
+++ app/test-helpers/httpmocks-products.js
@@ -0,0 +1,15 @@
+﻿var HttpMocks = HttpMocks || {};
+
+HttpMocks.product = {
+    empty: function (fixture) {
+        fixture.$httpBackend.when("GET", fixture.config.productsApiUrl)
+            .respond(200, []);
+    },
+    basicCars: function (fixture) {
+        fixture.$httpBackend.when("GET", fixture.config.productsApiUrl)
+            .respond(200, [
+                {name: "car 1", category: "car"},
+                {name: "car 2", category: "car"}
+            ]);
+    }
+};
